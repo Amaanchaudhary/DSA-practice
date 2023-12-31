4c695100595854222df1cdb947cd0cf821df9d21
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-12-31 23:22:52 +0530

    update

--- DSA-Qs-Bank-prac-27-12/Q10.js
+++ DSA-Qs-Bank-prac-27-12/Q10.js
@@ -8,8 +8,8 @@ var arr = [14 , 0 , 76 , 0 , 45 , 0 , 9 , 4 , 0 , 5]
 function removeZeros(arr){
     for(var i = 0 ; i < arr.length ; i++){
         if(arr[i] == 0){
-            arr.splice(i , 1 , undefined)
-            arr.push('zero')   //idk why this program is throwing error if i try to push 0 or '0 
+            arr.splice(i , 1 )
+            arr.push(0) 
         }
     }
     return arr
---
+++ Q9.js
@@ -0,0 +1,13 @@
+//Take string as input and return it in a proper camelCaseFormat
+//Eg:- 'BOB loves-coding'
+//output :- bobLovesCoding
+
+var str = "BOB loves-coding"
+
+function camelCase(str){
+
+
+    return str
+}
+
+console.log(camelCase(str))
