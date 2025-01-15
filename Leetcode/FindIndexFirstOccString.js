09c15019259986a8b9ba658f65b9b9582b6af571
amaanchaudhary <chaudharyamaan2121@gmail.com>
2025-01-15 11:03:12 +0530

    Find First Occurence in string problem solved

---
+++ Leetcode/FindIndexFirstOccString.js
@@ -0,0 +1,6 @@
+var strStr = function (haystack, needle) {
+    // indexOf is a string method which returns -1 if the given string not found in another string and if found it will return the first letter index of first occurence
+    return haystack.indexOf(needle)
+};
+
+console.log(strStr("anmaan", 'an'));
