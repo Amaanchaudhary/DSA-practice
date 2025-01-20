818236eee0031e0e1cbf13a5b7242e08e04f6dd2
AmaanChaudhary <chaudharyamaan982@gmail.com>
2025-01-20 12:37:40 +0530

    index.js and package json added

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 
 
-let nums = [1, 2, 3];
-nums.extra = "unexpected"; // Custom property
+var plusOne = function (digits) {
+    let length = digits.length //1
+    let find = 9
+    if (digits[length - 1] != 9) {
+        digits[length - 1] += 1
+        return digits
+    } else {
+        let i = length - 1 //0
+        while (find == 9 && i >= 0) {
+            if (digits[i] != 9) {
+                find = digits[i]
+                digits[i] += 1
+                i--
+            } else {
+                digits[i] = 0
+                i--
+            }
+        }
+        if (find != 9) {
+            digits.unshift(1)
+        }
+    }
+    return digits
+};
 
-for (let key in nums) {
-    console.log(key); // Outputs: 0, 1, 2, extra
-}
 
-for (let val of nums) {
-    console.log(val); // Outputs : 0, 1, 2
-}
+console.log(plusOne([9,9,9]));
 
 
