a960fae48d9bfddcaf7b304b24343f8af234e0d3
AmaanChaudhary <chaudharyamaan982@gmail.com>
2025-01-29 13:38:53 +0530

    problem sollved maxPrice

---
+++ Leetcode/maxProfit.js
@@ -0,0 +1,14 @@
+function maxProfit (prices){
+    let min_price = Infinity
+    let max_Profit = 0
+    for(let price of prices){
+        if(price < min_price){
+            min_price = price
+        }else if(price - min_price > max_Profit){
+            max_Profit = price - min_price
+        }
+    }
+    return max_Profit
+}
+
+console.log(maxProfit([7,8,4, 6, 1]));
